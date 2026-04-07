9816f8974c04dd13bc8d220b9ef5a6d1afd9193d
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2026-04-07 12:11:58 +0200

    Add base menus

--- Scenes/main_scene.gd
+++ Scenes/main_scene.gd
@@ -18,3 +18,21 @@ func _ready() -> void:
 		HTTPClient.Method.METHOD_GET,
 		""
 	)
+	show_only_menu($Menus/TitleScreen)
+
+
+func show_only_menu(menu : Control) -> void :
+	for m in $Menus.get_children():
+		if m is CanvasItem :
+			m.hide()
+	menu.show()
+
+
+func _on_play_pressed() -> void:
+	show_only_menu($Menus/Game)
+
+func _on_rules_pressed() -> void:
+	show_only_menu($Menus/Rules)
+
+func _on_return_pressed() -> void:
+	show_only_menu($Menus/TitleScreen)
